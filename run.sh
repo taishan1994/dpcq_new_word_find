867409de9a86fa70bb38dabeab7ffa53848b5758
taishan1994 <461600371@qq.com>
2022-05-12 10:44:02 +0800

    commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python run_discover.py "data/斗破苍穹/斗破苍穹.txt" "reports" --latin 50 0 0 0 --bigram 20 80 0 1.5 --unigram_2 20 40 0 1 --unigram_3 20 41 0 1 --iteration 2 --verbose 2
